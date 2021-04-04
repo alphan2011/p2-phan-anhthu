b65489bd7378f7f029cef2cb3631092828f279db
Anh-Thu Phan <aphan@ufl.edu>
2021-04-03 23:02:47 -0700

    Create hamburger menu

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open('GET', 'http://api.airvisual.com/v2/nearest_city?key=536bcd9b-c208-4f46-9b2a-75bc3ed56ac1', true);
 xmlhttp.send();
+
+// Hamburger menu
+var menu = document.querySelector(".menu")
+var hamburger = document.querySelector(".hamburger")
+var xIcon = document.querySelector(".xIcon")
+var menuIcon = document.querySelector(".menuIcon")
+
+hamburger.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLinks")
+
+menuLinks.forEach(
+  function (menuLinks) {
+    menuLinks.addEventListener("click", toggleMenu)
+  }
+)
