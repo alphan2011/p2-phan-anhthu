d21d48bb7b99435cf83000c606adc83526674c4a
Anh-Thu Phan <aphan@ufl.edu>
2021-04-04 17:49:45 -0700

    Add read more button

--- js/main.js
+++ js/main.js
@@ -18,13 +18,23 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open('GET', 'http://api.airvisual.com/v2/nearest_city?key=536bcd9b-c208-4f46-9b2a-75bc3ed56ac1', true);
 xmlhttp.send();
 
-// Hamburger menu
-var menu = document.querySelector(".menu")
-var hamburger = document.querySelector(".hamburger")
-var xIcon = document.querySelector(".xIcon")
-var menuIcon = document.querySelector(".menuIcon")
+// Read More button
+var button = document.querySelector(".read");
+var para = document.querySelector(".para");
 
-hamburger.addEventListener("click", toggleMenu)
+button.addEventListener("click",
+  function() {
+    para.style.display = "block";
+  }, false
+);
+
+// Hamburger menu
+var menu = document.querySelector(".menu");
+var hamburger = document.querySelector(".hamburger");
+var xIcon = document.querySelector(".xIcon");
+var menuIcon = document.querySelector(".menuIcon");
+
+hamburger.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
@@ -36,15 +46,15 @@ function toggleMenu() {
     xIcon.style.display = "block";
     menuIcon.style.display = "none";
   }
-}
+};
 
-var menuLinks = document.querySelectorAll(".menuLinks")
+var menuLinks = document.querySelectorAll(".menuLinks");
 
 menuLinks.forEach(
   function (menuLinks) {
     menuLinks.addEventListener("click", toggleMenu)
   }
-)
+);
 
 // Pop-up ad
 window.addEventListener("load",
@@ -52,7 +62,7 @@ window.addEventListener("load",
     document.querySelector(".pop-up").classList.add("show");
     document.querySelector("main").style.opacity = "0.1";
   }, false
-)
+);
 
 var close = document.getElementById("exit");
 close.addEventListener("click",
@@ -61,4 +71,4 @@ close.addEventListener("click",
     document.querySelector("main").style.opacity = "1";
     close.style.display = "none";
   }, false
-)
+);
